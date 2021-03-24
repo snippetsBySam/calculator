5976fc8d7aa93c102eeee906c2f55c34b2ee7dd9
Sam <snippetsbysam@outlook.com>
2021-03-24 23:44:53 +0000

    add calc functions

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+// addition function
+function add(a, b) {
+    return a + b;
+}
+
+// substract
+function sub(a, b) {
+    return a - b;
+}
+
+// multiply
+function multiply(a, b) {
+    return a * b;
+}
+
+// division
+function divide(a, b) {
+    return a / b;
+}
