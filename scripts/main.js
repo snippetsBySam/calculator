d3d005cb72c91f17e843501039661492783f0151
Sam <snippetsbysam@outlook.com>
2021-03-25 00:13:26 +0000

    rename variables, add operator function

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,40 @@
 // addition function
-function add(a, b) {
-    return a + b;
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
 }
 
 // substract
-function sub(a, b) {
-    return a - b;
+function substract(firstNum, secondNum) {
+    return firstNum - secondNum;
 }
 
 // multiply
-function multiply(a, b) {
-    return a * b;
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
 }
 
 // division
-function divide(a, b) {
-    return a / b;
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
+
+// operate function
+function operate(operator, firstNum, secondNum) {
+    if (operator == "+") {
+        return add(firstNum, secondNum);
+    }
+    if (operator == "-") {
+        return substract(firstNum, secondNum);
+    }
+    if (operator == "*") {
+        return multiply(firstNum, secondNum);
+    }
+    if (operator == "/") {
+        if (b == 0) {
+            return "div by zero";
+        }
+        else {
+            return add(firstNum, secondNum);
+        }
+    }
 }
